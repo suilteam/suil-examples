c5555359e3dbbd5201a0f18c9018b3b718491486
Carter Mbotho <mmbotho.carter@gmail.com>
2019-12-17 01:54:01 -0500

    Checking in sawtooth SDK

---
+++ intkey/src/addresser.cpp
@@ -0,0 +1,77 @@
+//
+// Created by dc on 2019-12-17.
+//
+#include <suil/utils.h>
+#include "addresser.h"
+
+namespace suil::sawsdk::intkey {
+
+    constexpr size_t MERKLE_ADDRESS_LENGTH = 70;
+    constexpr size_t NAMESPACE_PREFIX_LENGTH = 6;
+
+
+    static bool isHex(const suil::String& str) {
+        suil::strview  view = str;
+        return view.find_first_not_of("0123456789abcdef") == std::string::npos;
+    }
+
+    static void isValidAddr(const suil::String& addr) {
+        if (addr.size() != MERKLE_ADDRESS_LENGTH) {
+            throw Exception::create("Address size is invalid, {Expected: ",
+                    MERKLE_ADDRESS_LENGTH, ", Got: ", addr.size(), "}");
+        }
+        else if (!isHex(addr)) {
+            throw Exception::create("Address must be lowercase hexadecimal ONLY");
+        }
+    }
+
+    static void isNamespaceValid(const suil::String& ns) {
+        if (ns.size() != NAMESPACE_PREFIX_LENGTH) {
+            throw Exception::create("Namespace size is invalid, {Expected: ",
+                    NAMESPACE_PREFIX_LENGTH, ", Got: ", ns.size(), "}");
+        }
+        else if (!isHex(ns)) {
+            throw Exception::create("Namespace prefix must be lowercase hexadecimal ONLY");
+        }
+    }
+
+    Addresser::Addresser(const suil::String &ns)
+        : mNamespace(ns.dup())
+    {}
+
+    Addresser::Addresser(Addresser &&other) noexcept
+        : mNamespace(std::move(other.mNamespace)),
+          mNsPrefix(std::move(other.mNsPrefix))
+    {}
+
+    Addresser& Addresser::operator=(suil::sawsdk::intkey::Addresser &&other) noexcept
+    {
+        Ego.mNamespace = std::move(other.mNamespace);
+        Ego.mNsPrefix = std::move(other.mNsPrefix);
+        return Ego;
+    }
+
+    suil::String Addresser::mapKey(const suil::String &key, size_t pos, size_t count)
+    {
+        return utils::SHA512(key).substr(pos, count);
+    }
+
+    suil::String Addresser::mapNamespace(const suil::String &ns) const
+    {
+        return utils::SHA512(ns).substr(0, NAMESPACE_PREFIX_LENGTH);
+    }
+
+    const suil::String& Addresser::getNamespacePrefix() {
+        if (Ego.mNsPrefix.empty()) {
+            Ego.mNsPrefix = Ego.mapNamespace(Ego.mNamespace);
+            isNamespaceValid(Ego.mNsPrefix);
+        }
+        return Ego.mNsPrefix;
+    }
+
+    suil::String Addresser::makeAddress(const suil::String& key, size_t pos, size_t count) {
+        auto addr = utils::catstr(Ego.getNamespacePrefix(), Ego.mapKey(key, pos, count));
+        isValidAddr(addr);
+        return addr;
+    }
+}
---
+++ intkey/src/main.cc
@@ -0,0 +1,153 @@
+#include <suil/init.h>
+#include <suil/cmdl.h>
+#include <suil/sawtooth/sdk.h>
+#include <suil/json.h>
+#include "addresser.h"
+
+#define URL_PREFIX       "tcp://"
+#define URL_PREFIX_LEN   6
+#define URL_DEFAULT      "tcp://127.0.0.1:4004"
+
+#define MIN_VALUE        0x0
+#define MAX_VALUE        0xffffffff
+
+using namespace suil;
+
+define_log_tag(SAWTOOTH_INTKEY);
+
+static const suil::String INTKEY_NAMESPACE{"intkey"};
+
+struct IntkeyTransactor : public sawsdk::Transactor, LOGGER(SAWTOOTH_INTKEY) {
+
+    IntkeyTransactor(sawsdk::intkey::Addresser& addresser, sawsdk::Transaction&& txn, sawsdk::GlobalState&& state)
+        : sawsdk::Transactor(std::move(txn), std::move(state)),
+          mAddresser(addresser)
+    {}
+
+
+    void apply() override  {
+        idebug("IntKey apply");
+        size_t size{Ego.mTxn.payload().size()};
+        json::Object obj = json::Object::decode(
+                reinterpret_cast<const char *>(Ego.mTxn.payload().data()), size);
+
+        auto verb = (String) obj("Verb", false);
+        if (verb.empty()) {
+            throw Exception::create(sawsdk::Errors::InvalidTransaction, "Verb required");
+        }
+
+        auto name = (String) obj("Name", false);
+        if (name.empty()) {
+            throw Exception::create(sawsdk::Errors::InvalidTransaction, "Name required");
+        }
+        else if (name.size() > 20) {
+            throw Exception::create(sawsdk::Errors::InvalidTransaction, "Name too long, maximum 20 characters");
+        }
+
+        auto vp = obj("Value", false);
+        if (!vp) {
+            throw Exception::create(sawsdk::Errors::InvalidTransaction, "Value required");
+        }
+        auto value = (uint32_t) vp;
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw Exception::create(sawsdk::Errors::InvalidTransaction, "Value '",
+                    value, " is out range [", MIN_VALUE, " - ", MAX_VALUE, "]");
+        }
+
+        if (verb == "set") {
+            Ego.doSet(name, value);
+        }
+        else if (verb == "dec") {
+            Ego.doDec(name, value);
+        }
+        else if (verb == "inc") {
+            Ego.doInc(name, value);
+        }
+        else {
+            throw Exception::create(sawsdk::Errors::InvalidTransaction,
+                    "Invalid verb '", verb, "'");
+        }
+    }
+private:
+    inline suil::String makeAddress(const suil::String& name) {
+        return mAddresser.makeAddress(name, 64, std::string::npos);
+    }
+
+    void doSet(const suil::String& name, uint32_t value) {
+        auto addr = Ego.makeAddress(name);
+        idebug("Intkey::Transactor set {name: %s, address: %s, value: %s}", name(), addr(), value);
+
+        auto state = Ego.mState.getState(addr);
+        json::Object obj;
+        if (!state.empty()) {
+            obj = json::Object::decode(state);
+            auto stateValue = obj(name(), false);
+            if (stateValue) {
+                auto num = (uint32_t) stateValue;
+                throw Exception::create("Invalid set verb, already exists {name: ", name, ", value: ", num, "}");
+            }
+        }
+        obj.set(name(), value);
+        auto str = json::encode(obj);
+        Ego.mState.setState(addr, sawsdk::fromStdString(str));
+    }
+
+    void doDec(const suil::String& name, uint32_t value) {
+
+    }
+
+    void doInc(const suil::String& name, uint32_t value) {
+
+    }
+
+private:
+    sawsdk::intkey::Addresser& mAddresser;
+
+};
+
+struct IntKeyHandler : public sawsdk::TransactionHandler, LOGGER(SAWTOOTH_INTKEY) {
+    IntKeyHandler()
+        : mAddresser(INTKEY_NAMESPACE),
+          mNsPrefix(mAddresser.getNamespacePrefix())
+    {}
+
+    suil::String getFamilyName() const {
+        static const suil::String FAMILY_NAME{"intkey"};
+        return FAMILY_NAME.peek();
+    }
+
+    std::vector<suil::String> getVersions() const {
+        return {"1.0"};
+    }
+
+    std::vector<suil::String> getNamespaces() const {
+        return {mNsPrefix.peek()};
+    }
+
+    sawsdk::Transactor::Ptr getTransactor(sawsdk::Transaction&& txn, sawsdk::GlobalState&& state) {
+        return sawsdk::Transactor::Ptr(new IntkeyTransactor(mAddresser, std::move(txn), std::move(state)));
+    }
+
+private:
+    sawsdk::intkey::Addresser mAddresser;
+    suil::String mNsPrefix;
+};
+
+int main(int argc, char *argv[])
+{
+    suil::init(opt(printinfo, false));
+    log::setup(opt(verbose, 4));
+
+    try {
+        sawsdk::TransactionHandler::UPtr handler(new IntKeyHandler);
+        sawsdk::TransactionProcessor tp(URL_DEFAULT);
+        tp.registerHandler(std::move(handler));
+    }
+    catch (...) {
+        auto ex = Exception::fromCurrent();
+        serror("%s", ex.what());
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
